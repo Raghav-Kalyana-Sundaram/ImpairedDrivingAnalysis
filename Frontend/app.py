bb68b208966af7a40fb1458e88d9b3e0cb47e39e
Raghav-Kalyana-Sundaram <raghavkalyan25@outlook.com>
2022-05-18 12:03:28 -0700

    testing images

--- Frontend/app.py
+++ Frontend/app.py
@@ -5,20 +5,8 @@ import numpy as np
 
 st.title("Impaired Driving Analysis") 
 st.subheader("Graphs and Analysis") 
-progress_bar = st.sidebar.progress(0)
-status_text = st.sidebar.empty()
-last_rows = np.random.randn(1, 1)
-chart = st.line_chart(last_rows)
 
-for i in range(1, 101):
-    new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-    status_text.text("%i%% Complete" % i)
-    chart.add_rows(new_rows)
-    progress_bar.progress(i)
-    last_rows = new_rows
-    time.sleep(0.05)
-
-progress_bar.empty()
+graph1=st.image('Frontend\Images\Image1.jpg')
 
 with st.sidebar:
     st.radio ('Select one' , [1,2]) 
