a685e4a0c06df69ad336d428c5c58bdf8ad69328
Raghav-Kalyana-Sundaram <raghavkalyan25@outlook.com>
2022-05-18 11:22:45 -0700

    master

--- Frontend/app.py
+++ Frontend/app.py
@@ -2,13 +2,23 @@
 import streamlit as st
 import time
 import numpy as np
-from PIL import Image
-import requests
-import shutil
 
+progress_bar = st.sidebar.progress(0)
+status_text = st.sidebar.empty()
+last_rows = np.random.randn(1, 1)
+chart = st.line_chart(last_rows)
 
-st.title('Impaired Driving Reduction') 
-st.subheader('Paste links to different images of drivers around the world to see if they could be impaired') 
-image=Image.open('Jonny-Bairstow-batting-semifinal-match-England-Australia-2019.webp')
-st.image(image, caption='Bairstow') 
-image_url=st.text_area('Paste the link to the image here', value='Please make sure the image is free for commercial use and does not require any attribution', height=25, max_chars=100)
+for i in range(1, 101):
+    new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+    status_text.text("%i%% Complete" % i)
+    chart.add_rows(new_rows)
+    progress_bar.progress(i)
+    last_rows = new_rows
+    time.sleep(0.05)
+
+progress_bar.empty()
+
+# Streamlit widgets automatically run the script from top to bottom. Since
+# this button is not connected to any other logic, it just causes a plain
+# rerun.
+st.button("Re-run")
